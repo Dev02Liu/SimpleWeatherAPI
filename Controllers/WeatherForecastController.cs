739c65fca2f9ad4e918fa5a53e8002dc4c9f6e4d
Dev02Liu <mail@yijie-liu.com>
2024-05-01 18:26:14 +0200

    First commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WeatherForecastApi.Models;
+
+namespace WeatherForecastApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private enum Summaries 
+        { 
+            None = 0,
+            Cold,
+            Warm,
+            Hot
+        }
+
+        private Summaries GetSummaryBasedOnTemperature(int temperature)
+        {
+            if (temperature >= 0 && temperature <= 10)
+                return Summaries.Cold;
+            else if (temperature >= 11 && temperature <= 20)
+                return Summaries.Warm;
+            else if (temperature >= 21 && temperature <= 30)
+                return Summaries.Hot;
+
+            return Summaries.None;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecastClass> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(0, 5).Select(index =>
+            {
+                var temperatureC = rng.Next(0, 31); 
+                return new WeatherForecastClass
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = temperatureC,
+                    Summary = GetSummaryBasedOnTemperature(temperatureC).ToString()
+                };
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Models/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WeatherForecastApi.Models
+{
+    public class WeatherForecastClass
+    {
+        public DateTime Date { get; set; }
+        public float TemperatureC { get; set; }
+        public string? Summary { get; set; }
+
+
+    }
+}
